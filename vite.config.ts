f433faa6430b5d9a3e67964996ba6466e99cb351
Zulfiqar Rahman Aji <zulfiqar.aji@gmail.com>
2025-06-18 05:41:10 +0700

    Deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: "/Portofolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
