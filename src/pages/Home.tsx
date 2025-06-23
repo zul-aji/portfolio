b924bde87d3b6e360e7f58014e1d6c863d196e84
Zulfiqar Rahman Aji <zulfiqar.aji@gmail.com>
2025-06-23 15:52:04 +0700

    Ultra minor update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -248,7 +248,7 @@ function Home() {
                 <div
                     ref={contactRef}
                     data-section="contact"
-                    className="h-auto lg:min-h-screen w-screen px-10 pt-20 sm:pt-15 pb-10"
+                    className="h-full lg:min-h-screen w-screen px-10 pt-20 sm:pt-15 pb-10"
                 >
                     <ContactData/>
                 </div>
